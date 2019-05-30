ce854ec784fcd109c3e1d038d685fe44d2dfa94b
Rodrigo Gabriel <rodrigorgrb@gmail.com>
2019-05-30 12:47:14 +0000

    Template strings e corrigindo bugs do dateHelper

--- Alura/Formação Javascript/002 - JavaScript avançado I- ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/helpers/DateHelper.js
+++ Alura/Formação Javascript/002 - JavaScript avançado I- ES6, orientação a objetos e padrões de projetos/aluraframe/client/js/app/helpers/DateHelper.js
@@ -1,11 +1,18 @@
 class DateHelper{
 
+    constructor(){
+
+        throw new Error("Não é possivel instanciar esta classe");
+
+    }
+
     static textoParaData(texto){
         return new Date(new Date(...texto.split('-').map((item, indice) => item - indice % 2)));
     }
 
     static dataParaTexto(data){
-        return data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear();
+        if(!/\d{4}-\d{2}-\d{2}/.test(data)) throw new Error('Deve estar no formato aaa-mm-dd');
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
     }
 
 }
